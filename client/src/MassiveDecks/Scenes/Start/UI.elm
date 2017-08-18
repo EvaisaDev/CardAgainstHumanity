2829c6289cf6563348f16f82fca98fb2534ab941
EvaisaGiac <evagiacosa1@gmail.com>
2017-08-18 19:15:36 +0200

    hello? Site why u no work?

--- client/src/MassiveDecks/Scenes/Start/UI.elm
+++ client/src/MassiveDecks/Scenes/Start/UI.elm
@@ -61,7 +61,7 @@ view model =
         div [ id "start-screen" ]
             [ div [ id "start-screen-content", class "mui-panel" ]
                 (
-                    [ div [ id "logotop", class "logotop" ]()]
+                    [ div [ id "logotop", class "logotop" ] [text "Hello!"] ]
                     ++ (existingGames model.storage)
                     ++ [ Input.view model.nameInput
                        ]
